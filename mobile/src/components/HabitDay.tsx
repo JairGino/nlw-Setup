d88508a1b0317bb27e98bc371c3ae1d38b34b4a3
Jair Gino <jairgino@hotmail.com>
2023-02-24 17:57:15 -0300

    connect interface to API

--- mobile/src/components/HabitDay.tsx
+++ mobile/src/components/HabitDay.tsx
@@ -1,4 +1,8 @@
 import { Dimensions, TouchableOpacity, TouchableOpacityProps } from "react-native";
+import clsx from "clsx"
+
+import { generateProgressPercentage } from "../utils/generateProgressPercentage";
+import dayjs from "dayjs";
 
 const WEEK_DAYS = 7
 const SCREEN_HORIZONTAL_PADDING = (32 * 2) / 5;
@@ -6,12 +10,28 @@ const SCREEN_HORIZONTAL_PADDING = (32 * 2) / 5;
 export const DAY_MARGIN_BETWEEN = 8;
 export const DAY_SIZE = (Dimensions.get('screen').width / WEEK_DAYS) - (SCREEN_HORIZONTAL_PADDING + 5)
 
-interface Props extends TouchableOpacityProps {}
+interface Props extends TouchableOpacityProps {
+  date: Date;
+  amountOfHabits?: number;
+  amountCompleted?: number; 
+}
 
-export function HabitDay({ ...rest }: Props) {
+export function HabitDay({date, amountOfHabits = 0, amountCompleted = 0, ...rest }: Props) {
+  const amountAccomplishedPertentage = amountOfHabits > 0 ? generateProgressPercentage(amountOfHabits, amountCompleted) : 0
+  const today = dayjs().startOf('day').toDate()
+  const isCurrentDay = dayjs(date).isSame(today, 'day')
+  
   return (
     <TouchableOpacity
-      className="bg-zinc-900 border-2 rounded-lg border-zinc-800 m-1 "
+      className={clsx("border-2 rounded-lg m-1", {
+        ["bg-zinc-900 border-zinc-800"] : amountAccomplishedPertentage === 0,
+        ["bg-violet-900 border-violet-700"] : amountAccomplishedPertentage > 0 && amountAccomplishedPertentage <= 20,
+        ["bg-violet-800 border-violet-600"] : amountAccomplishedPertentage > 20 && amountAccomplishedPertentage <= 40,
+        ["bg-violet-700 border-violet-500"] : amountAccomplishedPertentage > 40 && amountAccomplishedPertentage <= 60,
+        ["bg-violet-600 border-violet-500"] : amountAccomplishedPertentage > 60 && amountAccomplishedPertentage <= 80,
+        ["bg-violet-500 border-violet-400"] : amountAccomplishedPertentage > 80,
+        ["border-white border-4"]: isCurrentDay
+      })}
       style={{ width: DAY_SIZE, height: DAY_SIZE }}
       activeOpacity={0.5}
       {...rest}
