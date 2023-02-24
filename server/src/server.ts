b579769a74be2bf2b2668b179d3651600a03037a
Jair Gino <jairgino@hotmail.com>
2023-02-24 17:53:57 -0300

    fix cors, allow resquest api from any ip host

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,16 +2,20 @@ import Fastify from 'fastify'
 import cors from '@fastify/cors'
 import { appRoutes } from './routes'
 
-const app = Fastify()
+const app = Fastify({
+  logger: true
+})
 
 app.register(cors, {
-  origin: ['http://localhost:3000']
+  origin: '*'
+  // origin: ['http://localhost:5173']
 })
 
 app.register(appRoutes)
 
 app.listen({
-  port: 3333
+  port: 3333,
+  host: '0.0.0.0'
 }).then(() => {
   console.log('HTTP Server running!')
 })
